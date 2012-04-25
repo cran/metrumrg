9345db0b9ab78562ec6420a505d8ed42a7d3d273
Tim Bergsma <timb@metrumrg.com>
2012-04-25 00:00:00 +0000

    version 5.45

--- R/constant.R
+++ R/constant.R
@@ -10,6 +10,23 @@ duplicated(
 	as.data.frame(a)
 )
 
+.crosses <- function(a, b, fromLast = FALSE){
+	if(!is.list(a)) a <- list(a)
+	if(!is.list(b)) b <- list(b)
+	a <- do.call(paste,c(a,list(sep='\r')))	
+	b <- do.call(paste,c(b,list(sep='\r')))	
+	a <- as.integer(factor(a))
+	b <- as.integer(factor(b))
+	if(fromLast) a <- rev(a)
+	if(fromLast) b <- rev(b)
+	i <- match(a,a) # indices for first matches
+	c <- b[i] # canonical
+	res <- b != c # does each value of b correspond to the canonical value (first within index) ?
+	if(fromLast) res <- rev(res)
+	res
+}
+	
+
 `crosses` <- function(a,b,all=FALSE)if(all) .crosses(a,b,FALSE) | .crosses(a,b,TRUE) else .crosses(a,b,FALSE)
 `%crosses%` <- function(a,b)any(crosses(a,b))
 `%crossed.on%` <- function(a,b)b %crosses% a
